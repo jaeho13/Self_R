5c6af6474f41b414af3a8be6ffd8b6e3bf752d35
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-04-08 03:01:19 +0900

    Add files via upload

--- Self_R/src/App.js
+++ Self_R/src/App.js
@@ -1,61 +1,61 @@
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 
-// import Home from "./component/Home";
-// import About from "./component/About";
-// import Second from "./component/Second";
+import Home from "./component/Home";
+import About from "./component/About";
+import Second from "./component/Second";
 
-// import Question1 from "./component/question/Question1";
-// import Question2 from "./component/question/Question2";
-// import Question3 from "./component/question/Question3";
-// import Question4 from "./component/question/Question4";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <nav>
-//       </nav>
-//       <Router>
-//       <Routes>
-//         <Route path="/question1" element={<Question1 />} />
-//         <Route path="/question2" element={<Question2 />} />
-//         <Route path="/question3" element={<Question3 />} />
-//         <Route path="/question4" element={<Question4 />} />
-//       </Routes>
-//       </Router>
-//       <Home />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import './App.css';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import ListBoardComponent from './component/Board/ListBoardComponent';
-import HeaderComponent from './component/Board/HeaderComponent';
-import FooterComponent from './component/Board/FooterComponent';
-import CreateBoardComponent from './component/Board/CreateBoardComponent';
-import ReadBoardComponent from './component/Board/ReadBoardComponent';
+import Question1 from "./component/question/Question1";
+import Question2 from "./component/question/Question2";
+import Question3 from "./component/question/Question3";
+import Question4 from "./component/question/Question4";
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <nav>
+      </nav>
       <Router>
-        <HeaderComponent/>
-          <div className="container">
-            <Routes>
-              <Route path ="/" exact element={<ListBoardComponent />}/>
-              <Route path ="/board" element={<ListBoardComponent />} />
-              <Route path="/create-board/:no" element={<CreateBoardComponent />} />
-              <Route path ="/read-board/:no" element={<ReadBoardComponent />} />
-            </Routes>
-          </div>
-        <FooterComponent/>
-    </Router>
+      <Routes>
+        <Route path="/question1" element={<Question1 />} />
+        <Route path="/question2" element={<Question2 />} />
+        <Route path="/question3" element={<Question3 />} />
+        <Route path="/question4" element={<Question4 />} />
+      </Routes>
+      </Router>
+      <Home />
     </div>
   );
 }
 
 export default App;
+
+// import './App.css';
+// import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+// import ListBoardComponent from './component/Board/ListBoardComponent';
+// import HeaderComponent from './component/Board/HeaderComponent';
+// import FooterComponent from './component/Board/FooterComponent';
+// import CreateBoardComponent from './component/Board/CreateBoardComponent';
+// import ReadBoardComponent from './component/Board/ReadBoardComponent';
+
+// function App() {
+//   return (
+//     <div>
+//       <Router>
+//         <HeaderComponent/>
+//           <div className="container">
+//             <Routes>
+//               <Route path ="/" exact element={<ListBoardComponent />}/>
+//               <Route path ="/board" element={<ListBoardComponent />} />
+//               <Route path="/create-board/:no" element={<CreateBoardComponent />} />
+//               <Route path ="/read-board/:no" element={<ReadBoardComponent />} />
+//             </Routes>
+//           </div>
+//         <FooterComponent/>
+//     </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
--- Self_R/src/component/Home.js
+++ Self_R/src/component/Home.js
@@ -2,7 +2,12 @@ import React from "react";
 import Styled from "styled-components";
 
 function Home() {
-    return <StyledHome>JBTI</StyledHome>
+    return (
+      <form>
+        <StyledHome>JBTI</StyledHome>
+        <StyledButton type="submit">시작하기</StyledButton>
+      </form>
+    );
 }
 
 export default Home;
@@ -12,3 +17,13 @@ const StyledHome = Styled.header`
   text-align: center;
   padding: 5px 0px 0px 0px;  
 `;
+
+const StyledButton = Styled.button`
+  font-size: 30px;
+  text-align: center;
+  padding-top: 500px;
+`;
+
+//시작하기 버튼이 왜 찹살떡마냥 늘어나는지 궁금하다.
+//패딩이란 위에와 그만큼 거리를 두는 것 아닌가??
+//text-align: center; 하면 가운데 정렬이 아닌가??
